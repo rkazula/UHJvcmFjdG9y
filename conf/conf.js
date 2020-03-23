a54d63a86e8bd7b92253fa5d56835dc7e6aa5cdc
Robert Kazuła <robert.kazula@makolab.com>
2020-03-23 21:10:00 +0100

    Added Allure reporting tool

--- conf/conf.js
+++ conf/conf.js
@@ -7,28 +7,43 @@
  * @License: MIT
  */
 
- var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
- var reporter = new HtmlScreenshotReporter({
-   dest: '../target/screenshots',
-   filename: 'testReport.html',
-   reportTitle: "TestRun Report",
-   showSummary: true,
-   showQuickLinks: true,
-   showConfiguration: true,
-   captureOnlyFailedSpecs: true,
-   preserveDirectory: true
- });
+var HtmlScreenshotReporter = require('protractor-jasmine2-screenshot-reporter');
+var reporter = new HtmlScreenshotReporter({
+  dest: '../target/screenshots',
+  filename: 'testReport.html',
+  reportTitle: "TestRun Report",
+  showSummary: true,
+  showQuickLinks: true,
+  showConfiguration: true,
+  captureOnlyFailedSpecs: true,
+  preserveDirectory: true
+});
+
+var AllureReporter = require('jasmine-allure-reporter');
+jasmine.getEnv().addReporter(new AllureReporter({
+  resultsDir: 'allure-results'
+}));
 
 exports.config = {
   directConnect: true,
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome',
+
+    chromeOptions: {
+      args: ["--headless", "--disable-gpu", "--window-size=1920,1080"]
+    }
   },
 
   // Framework to use. Jasmine is recommended.
-  framework: 'jasmine',
+  framework: 'jasmine2',
+  onPrepare: function() {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter({
+      resultsDir: 'allure-results'
+    }));
+  }
 
   // Spec patterns are relative to the current working directory when
   // protractor is called.
@@ -40,9 +55,9 @@ exports.config = {
 
   },
 
-    // Setup the report before any tests start
+  // Setup the report before any tests start
   beforeLaunch: function() {
-    return new Promise(function(resolve){
+    return new Promise(function(resolve) {
       reporter.beforeLaunch(resolve);
     });
   },
@@ -54,7 +69,7 @@ exports.config = {
 
   // Close the report after all tests finish
   afterLaunch: function(exitCode) {
-    return new Promise(function(resolve){
+    return new Promise(function(resolve) {
       reporter.afterLaunch(resolve.bind(this, exitCode));
     });
   }
